364b21735a0e32e1d1c62dcb2480a9d1d1e74777
Davee <38729175+DaveeIrungu@users.noreply.github.com>
2019-04-25 15:58:43 +0300

    Add files via upload
    Initial files. Code to be added after debugging is successful

---
+++ web_blocker.py
@@ -0,0 +1,11 @@
+import time
+
+# Path to hosts file as a raw string with the 'r prefix'
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+website_list = ["facebook.com", "www.facebook.com"]
+
+while True:
+
+    print(1)
+    time.sleep(5)
