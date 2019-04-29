517b56cab8679e0c057ffe0a1a12a02fbdda189b
Davee <38729175+DaveeIrungu@users.noreply.github.com>
2019-04-29 18:04:59 +0300

    Add files via upload
    Added functionality to block Instagram and Facebook between 8 am and 5 pm

--- web_blocker.py
+++ web_blocker.py
@@ -1,11 +1,43 @@
 import time
+from datetime import datetime as dt
 
 # Path to hosts file as a raw string with the 'r prefix'
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
-redirect = "127.0.0.1"
-website_list = ["facebook.com", "www.facebook.com"]
+# Path to temporal host, located on same directory
+hosts_temp = "hosts"
+redirect = "0.0.0.0"
+website_list = ["facebook.com", "www.facebook.com", "instagram.com", "www.instagram.com"]
 
 while True:
+    # Using the date time method, checks whether current time is between 8am and 4pm
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,
+                                                                          17):
+        # Check if lines already exist
+        # 'r+' allows both reading and writing
+        print("Working hours")
+        with open(hosts_path, 'r+') as file:
+            content = file.read()
+            # Iterate through the website list
+            for website in website_list:
+                if website in content:
+                    pass
+                # If e.g. facebook.com is already in the website_list, it will pass
+                # If website is present, it will write, as below:
+                else:
+                    file.write(redirect + " " + website + "\n")
 
-    print(1)
+
+
+    else:
+        with open(hosts_path, 'r+') as file:
+            # Use readline method instead that produces a list of all the lines in host file
+            content = file.readlines()
+            # file.seek(0) Places the cursor at the start of the line to enable truncate() method
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+
+        print("Leisure hours")
     time.sleep(5)
